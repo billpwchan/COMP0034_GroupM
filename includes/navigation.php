c6a8a548c45970dacb354e62e93614c2c263ccaf
Bill Chan <billpwchan@hotmail.com>
2019-01-23 09:04:04 +0000

    Added page template, modify navigation bar

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,47 +1,48 @@
-<nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<link rel="stylesheet" href="../assets/css/navigation.css" type="text/css">
+
+<nav class="navbar navbar-dark bg-dark navbar-expand-lg fixed-top">
     <a class="navbar-brand" href="#">
         <i class="fas fa-glass-cheers"></i>
         UberKidz
     </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+            aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
     <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>">
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item <?php if ($CURRENT_PAGE == "Index") { ?>active<?php } ?>">
                 <a class="nav-link" href="index.php">
                     <i class="fas fa-home"></i>
                     Home
-                    <span class="sr-only">(current)</span>
                 </a>
             </li>
-            <li class="nav-item <?php if ($CURRENT_PAGE == "MyAccount") {?>active<?php }?>">
+            <li class="nav-item <?php if ($CURRENT_PAGE == "MyAccount") { ?>active<?php } ?>">
                 <a class="nav-link" href="myAccount.php">
                     <i class="fas fa-user-circle"></i>
                     My Account
                 </a>
             </li>
-            <li class="nav-item <?php if ($CURRENT_PAGE == "Venues") {?>active<?php }?>">
+            <li class="nav-item <?php if ($CURRENT_PAGE == "Venues") { ?>active<?php } ?>">
                 <a class="nav-link" href="venues.php">
                     <i class="fas fa-map"></i>
                     Venues
                 </a>
             </li>
-            <li class="nav-item <?php if ($CURRENT_PAGE == "EntertainmentPackages") {?>active<?php }?>">
+            <li class="nav-item <?php if ($CURRENT_PAGE == "EntertainmentPackages") { ?>active<?php } ?>">
                 <a class="nav-link" href="events.php">
                     <i class="fas fa-map"></i>
                     Entertainment Packages
                 </a>
             </li>
-            <li class="nav-item <?php if ($CURRENT_PAGE == "Menus") {?>active<?php }?>">
+            <li class="nav-item <?php if ($CURRENT_PAGE == "Menus") { ?>active<?php } ?>">
                 <a class="nav-link" href="menus.php">
                     <i class="fab fa-elementor"></i>
                     Menus
                 </a>
             </li>
-            <li class="nav-item <?php if ($CURRENT_PAGE == "ContactUs") {?>active<?php }?>">
+            <li class="nav-item <?php if ($CURRENT_PAGE == "ContactUs") { ?>active<?php } ?>">
                 <a class="nav-link" href="contactUs.php">
                     <i class="fas fa-phone"></i>
                     Contact Us
---
+++ page-template.php
@@ -0,0 +1,11 @@
+<?php include("includes/config.php"); ?>
+<!doctype html>
+<html>
+<head>
+    <?php include("includes/headTags.php"); ?>
+</head>
+<body>
+<?php include("includes/navigation.php"); ?>
+</body>
+<?php include("includes/scripts.php"); ?>
+</html>
