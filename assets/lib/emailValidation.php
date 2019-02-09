ea3d8c94013e94f79d170ca45ffb5d9eb43e1b20
Bill Chan <billpwchan@hotmail.com>
2019-02-09 00:08:26 +0000

    Added email verification, success send email
    Signed-off-by: Bill Chan <billpwchan@hotmail.com>

--- assets/lib/emailValidation.php
+++ assets/lib/emailValidation.php
@@ -7,45 +7,95 @@
  */
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '/assets/controllers/dbConnect.php';
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php')) {
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+}
 
 class emailValidation
 {
-    protected $db;
+    protected $connect;
 
     function __construct()
     {
-        $this->db = db_connect();
+        $this->connect = db_connect();
     }
 
     /**
      * register new user
      *
-     * @param $first_name
-     * @param $last_name
+     * @param $firstName
+     * @param $lastName
+     * @param $gender
      * @param $email
      * @param $password
-     *
+     * @param $contactNumber
+     * @param $avatar
+     * @param $accountType
      * @return bool
      */
-    public function Register($first_name, $last_name, $email, $password)
+    public function Register($firstName, $lastName, $gender, $email, $password, $contactNumber, $avatar, $accountType)
     {
-        $first_name = mysqli_real_escape_string($this->db, $first_name);
-        $last_name = mysqli_real_escape_string($this->db, $last_name);
-        $email = mysqli_real_escape_string($this->db, $email);
-        $password = mysqli_real_escape_string($this->db, $password);
-        $password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 11]);
-        $email_activation_key = md5($email . $first_name . $last_name);
+        $firstName = mysqli_real_escape_string($this->connect, $firstName);
+        $lastName = mysqli_real_escape_string($this->connect, $lastName);
+        $gender = mysqli_real_escape_string($this->connect, $gender);
+        $email = mysqli_real_escape_string($this->connect, $email);
+        $sql = "select * from user WHERE email_address = '{$email}'";
+        $result = db_select($sql);
+        if (sizeof($result) != 0) {
+            header("Location:../../registration.php?registration=duplicateEmail");
+        }
+        $password = mysqli_real_escape_string($this->connect, $password);
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $contactNumber = mysqli_real_escape_string($this->connect, $contactNumber);
+        $registrationDate = date('Y-m-d H:i:s');
+        $email_activation_key = md5($email . $firstName . $lastName);
+        $uploadDirectory = "../uploads/avatar/";
+        $errors = []; // Store all foreseen and unforseen errors here
+        $fileExtensions = ['jpeg', 'jpg', 'png']; // Get all the file extensions
+        $fileName = $avatar['name'];
+        $fileSize = $avatar['size'];
+        $fileTmpName = $avatar['tmp_name'];
+        $fileExtension = strtolower(end(explode('.', $fileName)));
 
-        $query = "INSERT INTO `user` (user_ID, first_name, last_name, gender, email_address, password, contact_number, registration_date, avatar, status, email_activation_key)
-                  VALUES ('$first_name', '$last_name', '$email', '$password', '$email_activation_key')";
-        if (!$result = mysqli_query($this->db, $query)) {
-            exit(mysqli_error($this->db));
+        $uploadPath = $uploadDirectory . basename($fileName);
+
+        if (!in_array($fileExtension, $fileExtensions)) {
+            $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+        }
+
+        if ($fileSize > 20000000) {
+            $errors[] = "This file is more than 20MB. Sorry, it has to be less than or equal to 20MB";
+        }
+
+        if (empty($errors)) {
+            $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+            if ($didUpload) {
+                echo "The file " . basename($fileName) . " has been uploaded";
+            } else {
+                echo "An error occurred somewhere.";
+            }
+        } else {
+            foreach ($errors as $error) {
+                echo $error . "These are the errors" . "\n";
+            }
+        }
+
+        $sql = "INSERT INTO user (first_name, last_name, gender, email_address, password, contact_number, registration_date, avatar, status, email_activation_key) VALUES ('$firstName', '$lastName', '$gender', '$email', '$password', '$contactNumber', '$registrationDate', '$fileName', 0, '$email_activation_key')";
+        $result = db_query($sql);   //$result contains the ID generated
+        $autoGenID = mysqli_insert_id($this->connect);
+        if (strtolower($accountType) == "customer") {
+            $sql = "INSERT INTO customer (user_ID, account_balance) VALUES ({$autoGenID}, 2000.00)";
+            $result = db_query($sql);
+        } else if (strtolower($accountType) == "service provider") {
+            $companyName = 'UberKidz Company';
+            $sql = "INSERT INTO servicesupplier (user_ID, company_name) VALUES ({$autoGenID}, '$companyName')";
+            $result = db_query($sql);
         }
 
         // send email
-        $this->sendEmail('Verify Your Email Address', $email, $first_name, $email_activation_key);
+        $this->sendEmail('Verify Your Email Address', $email, $firstName, $email_activation_key);
 
-        return TRUE;
+        return $result;
     }
 
     /**
@@ -58,9 +108,9 @@ class emailValidation
      */
     public function sendEmail($subject, $to, $name, $email_activation_key)
     {
-        $smtp_server = 'smtp.zoho.com';
-        $username = 'user@yourdomain.com';
-        $password = 'PASSWORD';
+        $smtp_server = 'smtp.gmail.com';
+        $username = 'uberkidzuk@gmail.com';
+        $password = 'uberkidz123456';
         $port = '465';
         $encryption = 'ssl';
 
@@ -68,25 +118,24 @@ class emailValidation
         $link = 'http://' . $_SERVER['SERVER_NAME'] . '/activation.php?key=' . $email_activation_key;
 
         // get the html email content
-        $html_content = file_get_contents('email/email_verification.html');
+        $html_content = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/assets/lib/email/email_verification.html');
         $html_content = preg_replace('/{link}/', $link, $html_content);
 
         // get plain email content
-        $plain_text = file_get_contents('email/email_verification.txt');
+        $plain_text = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/assets/lib/email/email_verification.txt');
         $plain_text = preg_replace('/{link}/', $link, $plain_text);
 
-        $message = Swift_Message::newInstance()
-            ->setSubject($subject)
-            ->setFrom(['user@yourdomain.com' => 'iTech Empires'])
+        $message = (new Swift_Message($subject))
+            ->setFrom(['uberkidzuk@gmail.com' => 'UberKidz'])
             ->setTo([$to => $name])
             ->setBody($html_content, 'text/html')// add html content
             ->addPart($plain_text, 'text/plain'); // Add plain text
 
-        $transport = Swift_SmtpTransport::newInstance($smtp_server, $port, $encryption)
+        $transport = (new Swift_SmtpTransport($smtp_server, $port, $encryption))
             ->setUsername($username)
             ->setPassword($password);
 
-        $mailer = Swift_Mailer::newInstance($transport);
+        $mailer = new Swift_Mailer($transport);
 
         $mailer->send($message);
     }
