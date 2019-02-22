08e9b9f75273e17aa818d7beab372cd3d437056b
Bill Chan <billpwchan@hotmail.com>
2019-02-22 12:36:38 +0000

    Updated EventDetail Logic to support menu and venue
    Signed-off-by: Bill Chan <billpwchan@hotmail.com>

--- assets/controllers/eventDetail.php
+++ assets/controllers/eventDetail.php
@@ -13,29 +13,57 @@ switch ($previousURL) {
     case 'events':
         $productType = 'entertainment';
         $entertainers = read_entertainer_detail($productID);
+        $productDetails = read_event_detail($productID, $productType);
         break;
     case 'menus':
         $productType = 'menu';
+        $menus = read_menuItem_detail($productID);
+        $productDetails = read_menu_detail($productID, $productType);
         break;
     case 'venues':
         $productType = 'venue';
+        $productDetails = read_venue_detail($productID, $productType);
         break;
     default:
         $productType = 'Invalid';
 }
-$productDetails = read_product_detail($productID, $productType);
-if (sizeof($productDetails) != 1) {
-    header("location:events.php?display=invalid");
+
+if (!isset($productDetails) || sizeof($productDetails) != 1) {
+    header("location:{$previousURL}.php?display=invalid");
 } else {
     $productDetails = $productDetails[0];
 }
 
-function read_product_detail($productID, $productType)
+function read_event_detail($productID, $productType)
 {
     $sql = "
     SELECT event.event_ID, event.provider_ID, event.event_type, event.name, event.price, event.description, event.created, event.eventimage1, event.eventimage2, event.eventimage3, entertainmentpackage.duration
     FROM event, entertainmentpackage
     WHERE event.event_ID = entertainmentpackage.event_ID
+    AND event.event_ID = {$productID}
+    AND event.event_type = '{$productType}'
+    ";
+    return db_select($sql);
+}
+
+function read_menu_detail($productID, $productType)
+{
+    $sql = "
+    SELECT event.event_ID, event.provider_ID, event.event_type, event.name, event.price, event.description, event.created, event.eventimage1, event.eventimage2, event.eventimage3, menu.duration
+    FROM event, menu
+    WHERE event.event_ID = menu.event_ID
+    AND event.event_ID = {$productID}
+    AND event.event_type = '{$productType}'
+    ";
+    return db_select($sql);
+}
+
+function read_venue_detail($productID, $productType)
+{
+    $sql = "
+    SELECT event.event_ID, event.provider_ID, event.event_type, event.name, event.price, event.description, event.created, event.eventimage1, event.eventimage2, event.eventimage3, address, capacity, region
+    FROM event, venue
+    WHERE event.event_ID = venue.event_ID
     AND event.event_ID = {$productID}
     AND event.event_type = '{$productType}'
     ";
@@ -54,3 +82,14 @@ function read_entertainer_detail($productID)
     return db_select($sql);
 }
 
+function read_menuItem_detail($productID)
+{
+    $sql = "
+    SELECT quantity, name
+    FROM menu, menumap, menuitem
+    WHERE menu.event_ID = menumap.event_ID
+    AND menumap.menuitem_ID = menuitem.menuitem_ID
+    AND menu.event_ID = {$productID}
+    ";
+    return db_select($sql);
+}
