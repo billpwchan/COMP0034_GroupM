ef5fb814b02d4c26b76d75af2fae5b2bdea98cf6
Zhouming Sun <837575104@qq.com>
2019-02-22 14:18:11 +0000

    FAQ page TAC window

---
+++ includes/TAC.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html>
+<head>
+    <title>Terms and Conditions of www.uberkidz.com - TermsFeed</title>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <meta name="robots" content="noindex" />
+
+    <link href="https://termsfeed.com/assets/css/livepages.min.css?v=1545261057" rel="stylesheet" type="text/css" />
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-10195353-21', 'termsfeed.com');
+        ga('send', 'pageview');
+    </script>
+
+</head>
+<body class="live-pages">
+<div id="wrap">
+
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>
+                        Uberkidz
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-agreement">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="tab-content">
+                        <h1>Terms and Conditions </h1>
+
+
+                        <p>Last updated: February 13, 2019</p>
+
+
+                        <p>Please read these Terms and Conditions  carefully before using the www.uberkidz.com website  operated by Uberkidz.</p>
+
+                        <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
+
+                        <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. The Terms and Conditions agreement  for Uberkidz has been created with the help of <a href="https://termsfeed.com/">TermsFeed</a>.</p>
+
+
+                        <h2>Accounts</h2>
+
+                        <p>When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</p>
+
+                        <p>You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.</p>
+
+                        <p>You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
+
+
+                        <h2>Links To Other Web Sites</h2>
+
+                        <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by Uberkidz.</p>
+
+                        <p>Uberkidz has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Uberkidz shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
+
+                        <p>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
+
+
+                        <h2>Termination</h2>
+
+                        <p>We may terminate or suspend access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
+
+                        <p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+
+                        <p>We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</p>
+
+                        <p>Upon termination, your right to use the Service will immediately cease. If you wish to terminate your account, you may simply discontinue using the Service.</p>
+
+                        <p>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</p>
+
+
+                        <h2>Governing Law</h2>
+
+                        <p>These Terms shall be governed and construed in accordance with the laws of United Kingdom, without regard to its conflict of law provisions.</p>
+
+                        <p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</p>
+
+
+                        <h2>Changes</h2>
+
+                        <p>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</p>
+
+                        <p>By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</p>
+
+
+                        <h2>Contact Us</h2>
+
+                        <p>If you have any questions about these Terms, please contact us.</p>                            </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    Terms and Conditions of www.uberkidz.com
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<script type="text/javascript" src="https://termsfeed.com/assets/js/scripts.min.js?v=1545309672"></script>
+
+</body>
+</html>
