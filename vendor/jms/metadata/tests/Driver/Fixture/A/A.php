5895bade1afd15bb497e9d8e8f78bd35892a12fd
Bill Chan <billpwchan@hotmail.com>
2019-03-25 01:58:33 +0000

    Removed Vendor

--- vendor/jms/metadata/tests/Driver/Fixture/A/A.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Metadata\Tests\Driver\Fixture\A;
-
-class A
-{
-}
--- vendor/jms/metadata/tests/Driver/Fixture/C/SubDir/C.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Metadata\Tests\Driver\Fixture\C\SubDir;
-
-class C
-{
-}
